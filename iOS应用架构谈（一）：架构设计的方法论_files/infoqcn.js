dc2ba9bffac672a2e37c10b21e67d10548430209
hrzq19901209 <lol2012919@163.com>
2015-11-05 00:17:30 +0800

    iOS应用架构谈1

---
+++ iOS应用架构谈（一）：架构设计的方法论_files/infoqcn.js
@@ -0,0 +1,3 @@
+//此文件是给InfoQ中文站专用，勿删。修改请联系Gary
+$(".text_info .random_links").before('<div style="margin-bottom:5px"><b>【微信推荐】1.有内容的技术媒体，每天推送技术干货；2.来这里找技术同路人，共同推动技术变革；3.CTO分享技术观点，最新技术热点解读；4.希望每个技术细分领域内容这儿都找得到；5.听技术专家们讲关于他们技术团队的故事。<br/><center><img style="width: 200px;" src="http://q.infoqstatic.com/img/20151019/qrcode_for_infoq.jpg"/></center></b></div>')
+console.log('请关注infoq微信帐号:infoqchina');
